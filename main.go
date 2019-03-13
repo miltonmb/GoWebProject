6f5665d39c12b87124c95b3d9b3dd7e9935353fd
miltonmb <miltonmb@unitec.edu>
2019-03-13 15:24:25 -0600

    Calculo de totales medio funcional

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"sort"
@@ -70,7 +71,7 @@ func calcularTotal(Libros []Libro) float64 {
 
 	var counter = 1
 	for i := 0; i < len(Libros); i++ {
-		if Libros[i].ISBN != Libros[i+1].ISBN {
+		if Libros[i].ISBN == Libros[i+1].ISBN {
 			counter++
 		} else {
 			z = append(z, counter)
@@ -79,16 +80,141 @@ func calcularTotal(Libros []Libro) float64 {
 	}
 
 	for j := 0; j < len(z); j++ {
-		/*
-			z[0] = 3;
-			z[1] = 2;
-			z[2] = 1;
-			z[3] = 4;
-		*/
 
 	}
 
 	return temp
+}
+
+func calcularTotal(n []int) float64 {
+	var temp = n
+	sort.Slice(temp[:], func(i, j int) bool {
+		return temp[i] < temp[j]
+	})
+	fmt.Println(temp)
+	var total = 0.00
+	//Se toma el mayor elemento de la lista para
+	var iter = temp[len(temp)-1]
+	z := temp
+	x := []int{}
+
+	var acum = 0
+	for j := 0; j < iter; j++ {
+
+		if z[0] > 0 {
+			acum++
+			z[0]--
+		}
+
+		if z[1] > 0 {
+			acum++
+			z[1]--
+		}
+
+		if z[2] > 0 {
+			acum++
+			z[2]--
+		}
+
+		if z[3] > 0 {
+			acum++
+			z[3]--
+		}
+
+		if z[4] > 0 {
+			acum++
+			z[4]--
+		}
+		x = append(x, acum)
+		fmt.Println(x)
+		acum = 0
+	}
+
+	for k := 0; k < len(x); k++ {
+		if x[k] == 1 {
+			total += 800
+		}
+		if x[k] == 2 {
+			total += 2 * (800 - 800*0.05)
+		}
+		if x[k] == 3 {
+			total += 3 * (800 - 800*0.10)
+		}
+		if x[k] == 4 {
+			total += 4 * (800 - 800*0.20)
+		}
+		if x[k] == 5 {
+			total += 5 * (800 - 800*0.25)
+		}
+	}
+	return total
+}
+
+func calcularTotalEsp(n []int) float64 {
+	var temp = n
+	y := []int{}
+
+	sort.Slice(temp[:], func(i, j int) bool {
+		return temp[i] < temp[j]
+	})
+
+	if temp[0] == temp[1] && temp[2] == temp[0]*2 {
+		y = append(y, temp[0]*2, temp[2], temp[3], temp[4])
+	}
+
+	fmt.Println(n)
+	var total = 0.00
+	//Se toma el mayor elemento de la lista para
+	var iter = y[len(y)-1]
+	z := y
+	x := []int{}
+
+	var acum = 0
+	for j := 0; j < iter; j++ {
+
+		if z[0] > 0 {
+			acum++
+			z[0]--
+		}
+
+		if z[1] > 0 {
+			acum++
+			z[1]--
+		}
+
+		if z[2] > 0 {
+			acum++
+			z[2]--
+		}
+
+		if z[3] > 0 {
+			acum++
+			z[3]--
+		}
+
+		x = append(x, acum)
+		fmt.Println(x)
+		acum = 0
+	}
+
+	for k := 0; k < len(x); k++ {
+		if x[k] == 1 {
+			total += 800
+		}
+		if x[k] == 2 {
+			total += 2 * (800 - 800*0.05)
+		}
+		if x[k] == 3 {
+			total += 3 * (800 - 800*0.10)
+		}
+		if x[k] == 4 {
+			total += 4 * (800 - 800*0.20)
+		}
+		if x[k] == 5 {
+			total += 5 * (800 - 800*0.25)
+		}
+	}
+	return total
 }
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
