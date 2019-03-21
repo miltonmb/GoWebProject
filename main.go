f154bf10bd85144781f58c099358a42e04c7a846
miltonmb <miltonmb@unitec.edu>
2019-03-20 19:46:47 -0600

    metodo post de carrito listo

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 	//"sort"
 )
 
-var libro1 = makeLibro("libro1", "Learning Python, 4th Edition", 0, "O'Reilly Media, Inc.")
-var libro2 = makeLibro("libro2", "Learning Ruby, 1st Edition", 0, "O'Reilly Media, Inc.")
-var libro3 = makeLibro("libro3", "C# 7.0 in a Nutshell", 0, "O'Reilly Media, Inc.")
-var libro4 = makeLibro("libro4", "21st Century C, 2nd Edition", 0, "O'Reilly Media, Inc.")
-var libro5 = makeLibro("libro5", "Learning JavaScript, 3rd Edition", 0, "O'Reilly Media, Inc.")
+var libro1 = makeLibro("libro1", "Learning Python, 4th Edition", 10, "O'Reilly Media, Inc.")
+var libro2 = makeLibro("libro2", "Learning Ruby, 1st Edition", 10, "O'Reilly Media, Inc.")
+var libro3 = makeLibro("libro3", "C# 7.0 in a Nutshell", 10, "O'Reilly Media, Inc.")
+var libro4 = makeLibro("libro4", "21st Century C, 2nd Edition", 10, "O'Reilly Media, Inc.")
+var libro5 = makeLibro("libro5", "Learning JavaScript, 3rd Edition", 10, "O'Reilly Media, Inc.")
 var Arr = [5]Libro{libro1, libro2, libro3, libro4, libro5}
 var cart = [5]int{0, 0, 0, 0, 0}
 
@@ -138,80 +138,80 @@ func calcularTotal(n []int) float64 {
 }
 
 func calcularTotalEsp(n []int) float64 {
-	var test = []int{0,0,0,0,0}
-	  copy(test, n)
+	var test = []int{0, 0, 0, 0, 0}
+	copy(test, n)
 	var temp = test
-  
-	  sort.Slice(temp[:], func(i, j int) bool {
-		  return temp[i] < temp[j]
-	  })
-	
-	var y = []int{ }
-  
-	  if temp[0] == temp[1] && temp[2] == temp[0]*2 {
-		  y = append(y, temp[0]*2, temp[2], temp[3], temp[4])
-	  }
-  
-	if(len(y) < 3){
-	  return 0.0
+
+	sort.Slice(temp[:], func(i, j int) bool {
+		return temp[i] < temp[j]
+	})
+
+	var y = []int{}
+
+	if temp[0] == temp[1] && temp[2] == temp[0]*2 {
+		y = append(y, temp[0]*2, temp[2], temp[3], temp[4])
 	}
-  
-	  fmt.Println(y)
-	  var total = 0.00
-	  //Se toma el mayor elemento de la lista para
-	  var iter = y[len(y)-1]
-	  
+
+	if len(y) < 3 {
+		return 0.0
+	}
+
+	fmt.Println(y)
+	var total = 0.00
+	//Se toma el mayor elemento de la lista para
+	var iter = y[len(y)-1]
+
 	z := y
-  
-	  x := []int{}
-  
-	  var acum = 0
-	  for j := 0; j < iter; j++ {
-  
-		  if z[0] > 0 {
-			  acum++
-			  z[0]--
-		  }
-  
-		  if z[1] > 0 {
-			  acum++
-			  z[1]--
-		  }
-  
-		  if z[2] > 0 {
-			  acum++
-			  z[2]--
-		  }
-  
-		  if z[3] > 0 {
-			  acum++
-			  z[3]--
-		  }
-  
-		  x = append(x, acum)
-		  fmt.Println(x)
-		  acum = 0
-	  }
-  
-	  for k := 0; k < len(x); k++ {
-		  if x[k] == 1 {
-			  total += 800
-		  }
-		  if x[k] == 2 {
-			  total += 2 * (800 - 800*0.05)
-		  }
-		  if x[k] == 3 {
-			  total += 3 * (800 - 800*0.10)
-		  }
-		  if x[k] == 4 {
-			  total += 4 * (800 - 800*0.20)
-		  }
-		  if x[k] == 5 {
-			  total += 5 * (800 - 800*0.25)
-		  }
-	  }
-	  return total
-  }
+
+	x := []int{}
+
+	var acum = 0
+	for j := 0; j < iter; j++ {
+
+		if z[0] > 0 {
+			acum++
+			z[0]--
+		}
+
+		if z[1] > 0 {
+			acum++
+			z[1]--
+		}
+
+		if z[2] > 0 {
+			acum++
+			z[2]--
+		}
+
+		if z[3] > 0 {
+			acum++
+			z[3]--
+		}
+
+		x = append(x, acum)
+		fmt.Println(x)
+		acum = 0
+	}
+
+	for k := 0; k < len(x); k++ {
+		if x[k] == 1 {
+			total += 800
+		}
+		if x[k] == 2 {
+			total += 2 * (800 - 800*0.05)
+		}
+		if x[k] == 3 {
+			total += 3 * (800 - 800*0.10)
+		}
+		if x[k] == 4 {
+			total += 4 * (800 - 800*0.20)
+		}
+		if x[k] == 5 {
+			total += 5 * (800 - 800*0.25)
+		}
+	}
+	return total
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	p := newPage{Title: "Proyecto 2", Name1: Arr[0].name, Name2: Arr[1].name, Name3: Arr[2].name, Name4: Arr[3].name, Name5: Arr[4].name}
@@ -254,10 +254,12 @@ func bookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	for i := 0; i < 5; i++ {
 		if id == Arr[i].ISBN {
-			Arr[i].inStock = Arr[i].inStock - i1
-			cart[i] += i1
-			fmt.Fprintln(w, Arr[i].inStock)
-			break
+			if Arr[i].inStock > 0 {
+				Arr[i].inStock = Arr[i].inStock - i1
+				cart[i] += i1
+				fmt.Fprintln(w, Arr[i].inStock)
+				break
+			}
 		}
 	}
 }
